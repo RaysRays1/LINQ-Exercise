004d3db692ac5370af7bb0e440b635344e31ad9d
Raynard Brown <brown_raynard@icloud.com>
2024-09-03 11:43:53 -0500

    LINQEx1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("string list of games");
+            var games = new string[]
+                { "MLB The Show 24", "Red Dead 2", "John Madden 24", "NCAA Football 24", "Undisputed WBC" };
+            
+            //Filtering with LINQ
+            var orderNames = games.OrderByDescending(x => x.Length);
+
+            foreach (var game in orderNames)
+            {
+                Console.WriteLine(game);
+            }
+
         }
     }
 }
